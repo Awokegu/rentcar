774a79bc1ac89a47672a92c444bd2dd3864c37c7
Awoke <awoke8817@gmail.com>
2025-05-02 14:12:56 +0300

    update the term and privecy

--- resources/views/Terms_Conditions.blade.php
+++ resources/views/Terms_Conditions.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.myapp')
 @section('content')
     <div class="flex flex-col justify-center items-center gap-12 my-8 mx-auto max-w-screen-xl ">
-        <img src="/images/logos/LOGO.png" alt=" logo" class="w-[150px]">
+        <img src="/images/logos/LOGOtext.jpg" alt=" logo" class="w-[150px]">
         <h2 class="text-5xl font-medium text-center"> Terms & Conditions
         </h2>
         <div class=" text-start flex flex-col  gap-8 md:w-2/3 w-4/5">
@@ -53,14 +53,13 @@
             <h3 class="text-xl font-medium">
                 Governing Law:</h3>
             <p class=" ms-4">
-                These Terms & Conditions shall be governed by and construed in accordance with the laws of [Your
-                Country/State].
+                These Terms & Conditions shall be governed by and construed in accordance with the laws of Ethiopian State instutition.
             </p>
             <h3 class="text-xl font-medium">
                 Contact Us:</h3>
             <p class=" ms-4">
                 If you have any questions or concerns about our Privacy Policy or Terms & Conditions, please contact us at:
-                realrentcar@email.com
+                awoke8817@email.com
 
             </p>
         </div>
